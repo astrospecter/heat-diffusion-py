4381f37b5836f9c08df0ee9751bdf1d66d9f3ca0
Lina Zhu <lina.zhu0212@gmail.com>
2023-08-21 15:41:08 -0400

    initialization and diffusion funcs

--- heatmatrix.py
+++ heatmatrix.py
@@ -4,7 +4,7 @@ import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
 
 class HeatMatrix():
-    def __init__(self, amb_temp, size_x, size_y):
+    def __init__(self, amb_temp, size_x, size_y, diff_const):
         """
             Initializes temperature matrix temp.
 
@@ -12,10 +12,23 @@ class HeatMatrix():
             - amb_temp: the initial ambient temperature of the plate
             - size_x: the width of the plate
             - size_y: the height of the plate
+            - diff_const: the diffusion constant
         """
-        pass
+        self.temp = np.empty((size_x, size_y))
+        self.temp.fill(amb_temp)
+        self.temp_xlen = size_x
+        self.temp_ylen = size_y
+        self.diff_const = diff_const
+        self.time = 0
 
-    def add_heat_source(self, source_temp, source_x, source_y, size):
+    def init_heat_source(self, source_temp, source_x, source_y, size):
+        """Saves heat source information to the object."""
+        self.source_temp = source_temp
+        self.source_x = source_x
+        self.source_y = source_y
+        self.source_size = size
+
+    def add_heat_source(self):
         """
             Adds a heat source to the temperature matrix.
 
@@ -29,15 +42,18 @@ class HeatMatrix():
 
     def diffuse(self):
         """Diffuses the HeatMatrix object according to the diffusion equation one time step."""
-        pass
+        for i in range(1, self.temp_xlen - 1):
+            for j in range(1, self.temp_ylen - 1):
+                self.temp[i,j] = self.diff_const * (self.temp[i+1, j] + self.temp[i-1, j] + self.temp[i, j+1], self.temp[i, j-1] - 4*self.temp[i,j]) + self.temp[i,j]
 
-    def plot_heat_matrix(self, time):
+
+    def plot_heat_matrix(self):
         """Converts the heat matrix into a matplotlib pyplot."""
         # clear the plot
         plt.clf()
 
         # labels
-        plt.title(f"Temperature at t = {time}")
+        plt.title(f"Temperature at t = {self.time}")
         plt.xlabel("x")
         plt.ylabel("y")
 
